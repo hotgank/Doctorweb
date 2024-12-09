9001145cfe3e8a017e7798252783ced7979dee25
qwq995 <3087675598@qq.com>
2024-12-09 17:21:38 +0800

    https配置

--- Doctorweb/vue.config.js
+++ Doctorweb/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'https://localhost:8081',
+        target: 'https://zeropw.cn',
         changeOrigin: true
       }
     }
